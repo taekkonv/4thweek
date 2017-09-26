834e39d1ddc6b539185d0fbccaa43ed6644fe8bc
Snowfinger <taekkonv@gmail.com>
2017-09-26 14:06:43 +0900

    first commit

---
+++ bookmark/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView,DetailView,CreateView, UpdateView, DeleteView
+from .models import Bookmark
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.core.urlresolvers import reverse_lazy
+
+class BookmarkListV(ListView):
+    model = Bookmark
+class BookmarkDetailV(DetailView):
+    model = Bookmark
+class BookmarkCreateV(LoginRequiredMixin,CreateView):
+    model = Bookmark
+    fields = ['title','url']
+    success_url = reverse_lazy('bookmark:index')
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+
+        if form.is_valid:
+            form.instance.save()
+            return redirect('/bookmark/')
+        else:
+            return self.render_to_response({'form':form})
+
+class BookmarkUpdateV(LoginRequiredMixin,UpdateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+class BookmarkDeleteV(LoginRequiredMixin,DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:index')
